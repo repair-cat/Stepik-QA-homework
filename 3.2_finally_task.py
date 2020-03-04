bc949a5380f5756e2bc28abbfa67f29f14ad5e36
repair-cat <simf61@yandex.ru>
2020-03-04 20:47:47 +0500

    unittest finally task part 3.2

---
+++ 3.2_finally_task.py
@@ -0,0 +1,87 @@
+from selenium import webdriver
+import time
+import unittest
+
+class TestSite(unittest.TestCase):
+
+    def test_site_1(self): 
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        
+        input1 = browser.find_element_by_tag_name('div.first_block input.first')
+        input1.send_keys("Ivan")
+        input2 = browser.find_element_by_tag_name('div.first_block input.second')
+        input2.send_keys("Petrov")
+        input3 = browser.find_element_by_tag_name('div.first_block input.third')
+        input3.send_keys("goo@goo.com")
+        input4 = browser.find_element_by_tag_name('div.second_block input.first')
+        input4.send_keys("+7-999-982-111-21")
+        input5 = browser.find_element_by_tag_name('div.second_block input.second')
+        input5.send_keys("Russia")
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Not yet!")
+
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+    def test_site_2(self): 
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        
+        input1 = browser.find_element_by_tag_name('div.first_block input.first')
+        input1.send_keys("Ivan")
+        input2 = browser.find_element_by_tag_name('div.first_block input.second')
+        input2.send_keys("Petrov")
+        input3 = browser.find_element_by_tag_name('div.first_block input.third')
+        input3.send_keys("goo@goo.com")
+        input4 = browser.find_element_by_tag_name('div.second_block input.first')
+        input4.send_keys("+7-999-982-111-21")
+        input5 = browser.find_element_by_tag_name('div.second_block input.second')
+        input5.send_keys("Russia")
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Not yet!")
+
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
