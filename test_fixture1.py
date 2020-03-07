e032aa10b4936d615508aff063950dfd7e1f3fac
repair-cat <simf61@yandex.ru>
2020-03-07 16:44:58 +0500

    new fixture added

--- test_fixture1.py
+++ test_fixture1.py
@@ -1,43 +1,29 @@
+import pytest
 from selenium import webdriver
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 
+@pytest.fixture
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+@pytest.fixture(autouse=True)
+def prepare_data():
+    print()
+    print("preparing some critical data for every test")
+
+
 class TestMainPage1():
+    def test_guest_should_see_login_link(self, browser):
+        # не передаём как параметр фикстуру prepare_data, но она все равно выполняется
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
 
-    @classmethod
-    def setup_class(self):
-        print("\nstart browser for test suite..")
-        self.browser = webdriver.Chrome()
-
-    @classmethod
-    def teardown_class(self):
-        print("quit browser for test suite..")
-        self.browser.quit()
-
-    def test_guest_should_see_login_link(self):
-        self.browser.get(link)
-        self.browser.find_element_by_css_selector("#login_link")
-
-    def test_guest_should_see_basket_link_on_the_main_page(self):
-        self.browser.get(link)
-        self.browser.find_element_by_css_selector(".basket-mini .btn-group > a")
-
-
-class TestMainPage2():
-
-    def setup_method(self):
-        print("start browser for test..")
-        self.browser = webdriver.Chrome()
-
-    def teardown_method(self):
-        print("quit browser for test..")
-        self.browser.quit()
-
-    def test_guest_should_see_login_link(self):
-        self.browser.get(link)
-        self.browser.find_element_by_css_selector("#login_link")
-
-    def test_guest_should_see_basket_link_on_the_main_page(self):
-        self.browser.get(link)
-        self.browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
